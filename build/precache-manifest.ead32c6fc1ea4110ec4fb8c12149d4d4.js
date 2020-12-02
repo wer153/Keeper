2da260e200ff7cf915889f0f9cefcf11c10bb3db
Kim Minki <77wer153@gmail.com>
2020-12-02 10:25:22 +0900

    change font

---
+++ build/precache-manifest.ead32c6fc1ea4110ec4fb8c12149d4d4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bed131ecc9d1fd83cbcdcc58abad67",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "14dc137ce9e5dd8d0344",
+    "url": "/Keeper/static/js/2.08caa930.chunk.js"
+  },
+  {
+    "revision": "b24d50fc884e4f0b53fb",
+    "url": "/Keeper/static/js/main.3b9836fa.chunk.js"
+  },
+  {
+    "revision": "7e94f4bb0c0b87bffef0",
+    "url": "/Keeper/static/js/runtime-main.4f47787b.js"
+  }
+]);
