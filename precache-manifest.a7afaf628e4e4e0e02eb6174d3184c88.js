49aa5b70967ccbdf9e14f1dd4c32d6ce241f73d1
Kim Minki <77wer153@gmail.com>
2020-12-02 10:34:43 +0900

    Updates

---
+++ precache-manifest.a7afaf628e4e4e0e02eb6174d3184c88.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7070202b7e5e4e452a4d7940d12f15cf",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "14dc137ce9e5dd8d0344",
+    "url": "/Keeper/static/js/2.08caa930.chunk.js"
+  },
+  {
+    "revision": "b24d50fc884e4f0b53fb",
+    "url": "/Keeper/static/js/main.3b9836fa.chunk.js"
+  },
+  {
+    "revision": "7e94f4bb0c0b87bffef0",
+    "url": "/Keeper/static/js/runtime-main.4f47787b.js"
+  }
+]);
